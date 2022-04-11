d5bf7557a2e0003077c231321ab1c01d6b115547
Jermaine Walters <walte1jj@gmail.com>
2022-04-10 23:30:49 -0400

    prompts and questions completed

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Professional README Generator Starter Code
+# create readme
 
-[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
--- index.js
+++ index.js
@@ -1,13 +1,80 @@
 // TODO: Include packages needed for this application
+function init() {
+
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateReadme = require("./Develop/utils/generateMarkdown");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the name of your project?",
+            name: "title",
+        },
+        
+        {
+            type: "input",
+            message: "Provide a description of your project",
+            name: "description",
+        },
+
+        {
+            type: "input",
+            message: "How do you install your application?",
+            name: "installation",
+        },
+
+        {
+            type: "input",
+            message: "How do you use the application?",
+            name: "usage",
+        },
+        {
+            type: "input",
+            message: "Give guidlines on how someone can contribute to your project",
+            name: "contributing",
+        },
+
+        {
+            type: "input",
+            message: "Give instructions on testing",
+            name: "tests",
+        },
+
+        {
+            type: "checkbox",
+            message: "Choose a license for your project",
+            choices: ["Apache 2.0 License", "MIT", "Attribution License (BY)", "GNU GPL v3"],
+            name: "license",
+        },
+
+        {
+            type: "input",
+            message: "Enter your GitHub username",
+            name: "github",
+        },
+
+        {
+            type: "input",
+            message: "Enter your email address",
+            name: "email",
+        },
+    ])
+    .then((answers) => {
+        return fs.writeFileSync(path.join (process.cwd(), "README.md"), generateReadme(answers));
+    });
+}
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+
 
 // Function call to initialize app
 init();
